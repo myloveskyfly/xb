11dee709d3c8ecd32f17a552afe9c66b6514e5f3
myloveskyfly <73742142+myloveskyfly@users.noreply.github.com>
2023-07-06 23:02:46 +0800

    Update jd_xiequ_white.js

--- jd_xiequ_white.js
+++ jd_xiequ_white.js
@@ -1,21 +1,19 @@
-//携趣白名单更新 uid和vkey在白名单管理接口能查到，ukey在api提取链接内包含
-//参考定时 5,35 * * * *
-//变量 export XIEQU_CONFIG=“备注@uid@ukey@vkey”
-
 const axios = require('axios');
-const sendNotify = require('./sendNotify')
+const sendNotify = require('./sendNotify');
 
 // 配置青龙变量参数
 const [USER, UID, UKEY, VKEY] = process.env.XIEQU_CONFIG.split('@');
-
-const GET_IP_URL = `http://api.xiequ.cn/VAD/GetIp.aspx?act=get&uid=${UID}&vkey=${VKEY}&num=1&time=30&plat=1&re=0&type=0&so=1&ow=1&spl=3&addr=&db=1`;
+// 配置携趣变量参数
 const CLEAR_WHITE_LIST_URL = `http://op.xiequ.cn/IpWhiteList.aspx?uid=${UID}&ukey=${UKEY}&act=del&ip=all`;
 const ADD_TO_WHITE_LIST_URL = `http://op.xiequ.cn/IpWhiteList.aspx?uid=${UID}&ukey=${UKEY}&act=add`;
 const GET_WHITE_LIST_URL = `http://op.xiequ.cn/IpWhiteList.aspx?uid=${UID}&ukey=${UKEY}&act=get`;
+const GET_IP_URL = `http://api.xiequ.cn/VAD/GetIp.aspx?act=get&uid=${UID}&vkey=${VKEY}&num=1&time=30&plat=1&re=0&type=0&so=1&ow=1&spl=3&addr=&db=1`;
 
-// 设置请求的User-Agent头，以伪装成浏览器请求
+// 设置请求的User-Agent头，以伪装成浏览器请求，并禁止缓存
 axios.defaults.headers.common['User-Agent'] =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36';
+axios.defaults.headers.common['Cache-Control'] = 'no-cache';
+axios.defaults.headers.common['Pragma'] = 'no-cache';
 
 // 获取当前外网IP的函数
 async function getPublicIP() {
@@ -43,7 +41,7 @@ async function clearWhiteList() {
 // 更新白名单函数
 async function addToWhiteList(ip) {
   try {
-	console.log('✅执行更新白名单');
+    console.log('✅执行更新白名单');
     await axios.get(`${ADD_TO_WHITE_LIST_URL}&ip=${ip}`);
     console.log('✅更新白名单成功');
   } catch (error) {
@@ -76,29 +74,29 @@ async function checkAccountStatus() {
         await addToWhiteList(ip);
         const whiteList = await getWhiteList(); // 获取更新后的白名单
         console.log('😀更新后的白名单:', whiteList);
-        await sendNotify.sendNotify(`🎉携趣白名单更新通知🎉`,`当前外网IP变更为：\n${ip}\n\n账号：💎${USER}💎\n\n更新后的白名单为：\n${whiteList}`)
+        await sendNotify.sendNotify(`🎉携趣白名单更新通知🎉`,`当前外网IP变更为：\n${ip}\n\n账号：💎${USER}💎\n\n更新后的白名单为：\n${whiteList}`);
+        console.log('⏲10s后重新检查账号状态！');
+		await new Promise((resolve) => setTimeout(resolve, 10000)); // 10s后重新检查账号状态
+		await checkAccountStatus(); // 重新检查账号状态
       }
     } else if (content.includes('过期')) {
       console.log('🔔账号额度已经用完或者账号已过期！');
-      await sendNotify.sendNotify(`⚠通知⚠`,`\n\n账号：💎${USER}💎额度已经用完或者账户已过期！`)
+      await sendNotify.sendNotify(`⚠通知⚠`,`账号：💎${USER}💎\n额度已经用完或者账户已过期！`);
     } else {
       console.log('🔔无法解析账号状态');
-      await sendNotify.sendNotify(`🔔通知🔔`,`携趣代理更新：无法解析账号💎${USER}💎状态!`)
+      await sendNotify.sendNotify(`🔔通知🔔`,`携趣代理更新：无法解析账号💎${USER}💎状态!`);
     }
   } catch (error) {
     console.error('🔔检查账号状态失败:', error.message);
-    await sendNotify.sendNotify(`🔔通知🔔`,`携趣代理更新：检查账号💎${USER}💎状态失败！`)
+    await sendNotify.sendNotify(`🔔通知🔔`,`携趣代理更新：检查账号💎${USER}💎状态失败！`);
   }
 }
 
 // 入口函数
 async function main() {
-	const NO = process.env.XIEQU_CONFIG.split('@')[0];
-	console.log('💎当前账号名称为：', NO + '\n');
-  	await checkAccountStatus();
- 	console.log('⏲30s后重新检查账号状态！');
-	await new Promise((resolve) => setTimeout(resolve, 30000)); // 30s后重新检查账号状态
-	await checkAccountStatus(); // 重新检查账号状态
+  const NO = process.env.XIEQU_CONFIG.split('@')[0];
+  console.log('💎当前账号名称为：', NO + '\n');
+  await checkAccountStatus();
 }
 
 main();
